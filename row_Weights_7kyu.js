2a3ce924ecdc9d9fcdf1c85b4101e1ae6633b3b2
Vladimir Shits <rav9aa@gmail.com>
2021-10-12 20:26:06 +0300

    completed task - row_Weights_7kyu

---
+++ row_Weights_7kyu.js
@@ -0,0 +1,11 @@
+function rowWeights(array){
+    let result = [0, 0];
+    for(let i = 0; i < array.length; i++){
+        if(i % 2 == 0){
+            result[0] += array[i]
+        } else {
+            result[1] += array[i]
+        }
+    }
+    return result
+}
